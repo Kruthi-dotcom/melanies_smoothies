1a1891aeb0b7b7a4a67c6e606711a96280619819
Kruthi-dotcom <kruthigowdar@gmail.com>
2025-04-05 17:10:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
         st.write(f'The search value for {fruit_chosen} is {search_on}.')
         st.subheader(f'{fruit_chosen} Nutrition Information')
 
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/all{search_on}")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         if smoothiefroot_response.status_code == 200:
             st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         else:
